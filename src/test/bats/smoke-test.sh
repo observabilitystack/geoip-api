15c67f9ea12bd87e1be5e8e296f8ca9537614fc3
Torsten Bøgh Köster <tbk@thiswayup.de>
2023-03-19 23:51:16 +0100

    this might fix smoke test networking

--- src/test/bats/smoke-test.sh
+++ src/test/bats/smoke-test.sh
@@ -4,26 +4,26 @@ load /usr/lib/bats/bats-support/load.bash
 load /usr/lib/bats/bats-assert/load.bash
 
 @test "[meta] Healthcheck ok" {
-  curl -fs "http://localhost:8080/actuator/health"
+  curl -fs "http://geoip-api:8080/actuator/health"
 }
 
 @test "[meta] Prometheus metrics ok" {
-  curl -fs "http://localhost:8080/actuator/prometheus"
+  curl -fs "http://geoip-api:8080/actuator/prometheus"
 }
 
 @test "[meta] Prometheus metrics available" {
-  curl -fs "http://localhost:8080/actuator/prometheus"|grep "geoip_database"
+  curl -fs "http://geoip-api:8080/actuator/prometheus"|grep "geoip_database"
 }
 
 @test "[geoip] 8.8.8.8" {
-  run curl -fs "http://localhost:8080/8.8.8.8"
+  run curl -fs "http://geoip-api:8080/8.8.8.8"
   assert_success
   assert_output --partial '"country":"US"'
   assert_output --partial '"asnOrganization":"GOOGLE"'
 }
 
 @test "[geoip] 149.233.213.224" {
-  run curl -fs "http://localhost:8080/149.233.213.224"
+  run curl -fs "http://geoip-api:8080/149.233.213.224"
   assert_success
   assert_output --partial '"country":"DE"'
   assert_output --partial '"city":"Hamburg"'
